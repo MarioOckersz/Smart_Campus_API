7abd8e3a49f1e84bd97b452c497c32cd0a7468a0
MarioOckersz <marioockersz16@gmail.com>
2026-04-22 02:33:56 +0530

    data storage inbuilt

--- src/main/java/com/smartcampus/data/DataStore.java
+++ src/main/java/com/smartcampus/data/DataStore.java
@@ -2,11 +2,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package com.smartcampus.data;
 
 /**
  *
  * @author mario
  */
+
+
+
+
+import com.smartcampus.models.*;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class DataStore {
-    
+    private static final DataStore instance = new DataStore();
+    private final Map<String, Room> rooms = new ConcurrentHashMap<>();
+    private final Map<String, Sensor> sensors = new ConcurrentHashMap<>();
+
+    private DataStore() {}
+    public static DataStore getInstance() { return instance; }
+    public Map<String, Room> getRooms() { return rooms; }
+    public Map<String, Sensor> getSensors() { return sensors; }
 }
