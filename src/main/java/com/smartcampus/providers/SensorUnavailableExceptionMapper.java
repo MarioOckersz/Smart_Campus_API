6c3882e1aca2c1775135eea72bea394bec97ca29
MarioOckersz <marioockersz16@gmail.com>
2026-04-22 02:37:20 +0530

    sensor not avilable provider created

---
+++ src/main/java/com/smartcampus/providers/SensorUnavailableExceptionMapper.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus.providers;
+
+/**
+ *
+ * @author mario
+ */
+
+
+import com.smartcampus.exceptions.SensorUnavailableException;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class SensorUnavailableExceptionMapper implements ExceptionMapper<SensorUnavailableException> {
+    @Override
+    public Response toResponse(SensorUnavailableException exception) {
+        return Response.status(Response.Status.FORBIDDEN)
+                .entity(ResponseHelper.format(403, exception.getMessage()))
+                .build();
+    }
+}
