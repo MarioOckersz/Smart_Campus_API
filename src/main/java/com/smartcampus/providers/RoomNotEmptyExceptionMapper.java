65c3e10721bc51ab3b66bb7c6cd9a4d002a971eb
MarioOckersz <marioockersz16@gmail.com>
2026-04-22 02:37:05 +0530

    room not null provider created

---
+++ src/main/java/com/smartcampus/providers/RoomNotEmptyExceptionMapper.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus.providers;
+
+/**
+ *
+ * @author mario
+ */
+
+
+import com.smartcampus.exceptions.RoomNotEmptyException;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class RoomNotEmptyExceptionMapper implements ExceptionMapper<RoomNotEmptyException> {
+    @Override
+    public Response toResponse(RoomNotEmptyException exception) {
+        return Response.status(Response.Status.CONFLICT)
+                .entity(ResponseHelper.format(409, exception.getMessage()))
+                .build();
+    }
+}
