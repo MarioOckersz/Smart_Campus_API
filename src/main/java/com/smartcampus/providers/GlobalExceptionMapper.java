aebd04152fa263b986957383a819eded699d30a8
MarioOckersz <marioockersz16@gmail.com>
2026-04-22 02:35:55 +0530

    common error catcher created

---
+++ src/main/java/com/smartcampus/providers/GlobalExceptionMapper.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus.providers;
+
+/**
+ *
+ * @author mario
+ */
+
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class GlobalExceptionMapper implements ExceptionMapper<Throwable> {
+    @Override
+    public Response toResponse(Throwable e) {
+        return Response.status(500).entity(ResponseHelper.format(500, "Unexpected error.")).build();
+    }
+}
